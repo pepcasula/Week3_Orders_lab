f517c917e8cd2ab8ef89b481e4c49896578acb6d
peppino <peppinocasula@yahoo.it>
2022-07-06 13:53:45 +0100

    first commit

---
+++ models/order_list.py
@@ -0,0 +1,5 @@
+from models.order import Order
+
+order_1 = Order('Peppino', '06/07/2022', 'The wonderful thing', 1)
+order_2 = Order('Nathan', '05/07/2022', 'The wonderful thing 2', 2)
+order_3 = Order('Peppino', '06/07/2022', 'The wonderful thing - Origins', 1)
